cfa350d01b1ea66db5ecbe83ee51f00030eecb4e
Daniel Cheang <dcheang@sealimited.com>
2022-11-12 11:22:12 -0500

    added brake status card and release brake button

--- build/static/js/xlsx.157adab9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpstride=this.webpackJsonpstride||[]).push([[2],{445:function(n,i){},538:function(n,i){},539:function(n,i){}}]);
-//# sourceMappingURL=xlsx.157adab9.chunk.js.map
---
+++ build/static/js/xlsx.9985f231.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpstride=this.webpackJsonpstride||[]).push([[2],{462:function(n,i){},557:function(n,i){},558:function(n,i){}}]);
+//# sourceMappingURL=xlsx.9985f231.chunk.js.map
