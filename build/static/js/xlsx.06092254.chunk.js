763190b547265e0c2aba222a3990cd85e8c8541b
Nick Helber <ndhelber@yahoo.com>
2023-02-27 14:10:08 -0500

    Nick laptop test. Added battery temp.

---
+++ build/static/js/xlsx.06092254.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpstride=this.webpackJsonpstride||[]).push([[2],{460:function(n,i){},553:function(n,i){},554:function(n,i){}}]);
+//# sourceMappingURL=xlsx.06092254.chunk.js.map
--- build/static/js/xlsx.9985f231.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpstride=this.webpackJsonpstride||[]).push([[2],{462:function(n,i){},557:function(n,i){},558:function(n,i){}}]);
-//# sourceMappingURL=xlsx.9985f231.chunk.js.map
