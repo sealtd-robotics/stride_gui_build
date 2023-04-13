c3caf02e825a0c008220f943fcc4cd814bec4595
Nick Helber <nhelber@sealimited.com>
2023-04-13 16:30:08 -0400

    Added hasBrake variable/logic to GUi

--- build/static/js/xlsx.b43a975a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpstride=this.webpackJsonpstride||[]).push([[2],{459:function(n,i){},552:function(n,i){},553:function(n,i){}}]);
-//# sourceMappingURL=xlsx.b43a975a.chunk.js.map
---
+++ build/static/js/xlsx.cf5759e3.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpstride=this.webpackJsonpstride||[]).push([[2],{464:function(n,i){},557:function(n,i){},558:function(n,i){}}]);
+//# sourceMappingURL=xlsx.cf5759e3.chunk.js.map
