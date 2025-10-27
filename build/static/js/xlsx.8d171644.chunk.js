93ebfe139ccb04883541dda2e46a48f5284ad012
Nick Helber <nhelber@sealimited.com>
2025-10-27 10:26:47 -0400

    Remove spin and move buttons, update code version, and batt voltage to 1 decimal

--- build/static/js/xlsx.8d171644.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpstride=this.webpackJsonpstride||[]).push([[2],{472:function(n,i){},565:function(n,i){},566:function(n,i){}}]);
-//# sourceMappingURL=xlsx.8d171644.chunk.js.map
---
+++ build/static/js/xlsx.b31bc4ca.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpstride=this.webpackJsonpstride||[]).push([[2],{469:function(n,i){},562:function(n,i){},563:function(n,i){}}]);
+//# sourceMappingURL=xlsx.b31bc4ca.chunk.js.map
